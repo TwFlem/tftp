d8d6054eb06103616eb52d2703dd0b4c9473d456
twflem <twf.github@proton.me>
2024-09-01 00:03:06 -0400

    finish rest of packets

--- packet/packet.go
+++ packet/packet.go
@@ -8,14 +8,39 @@ import (
 )
 
 const (
-	maxFilenameSize  = 255
-	opPacketByteSize = 2
+	maxFilenameSize = 255
 )
 
 var (
-	errInvalidOp       = errors.New("invalid_op")
-	errInvalidMode     = errors.New("invalid_mode")
-	errMalformedPacket = errors.New("invalid_packet")
+	ErrFileNotFound = Error{
+		Code:    1,
+		Message: "file_not_found",
+	}
+	ErrAccessViolation = Error{
+		Code:    2,
+		Message: "access_violation",
+	}
+	ErrAllocationExceeded = Error{
+		Code:    3,
+		Message: "disk_full_or_allocation_exceeded",
+	}
+	ErrInvalidOperation = Error{
+		Code:    4,
+		Message: "invalid_tftp_operation",
+	}
+	// ErrInvalidTransferID invalid, unkown, or in use ports.
+	ErrInvalidTransferID = Error{
+		Code:    5,
+		Message: "invalid_transfer_id",
+	}
+	ErrFileAlreadyExists = Error{
+		Code:    6,
+		Message: "file_already_exists",
+	}
+	ErrInvalidUser = Error{
+		Code:    7,
+		Message: "invalid_user",
+	}
 )
 
 type Op int
@@ -48,12 +73,12 @@ func (o Op) String() string {
 
 func OpFrom(packet []byte) (Op, error) {
 	if len(packet) < 2 {
-		return OpInvalid, fmt.Errorf("not enough bytes for an op code: %w", errInvalidOp)
+		return OpInvalid, fmt.Errorf("not enough bytes for an op code: %w", ErrInvalidOperation)
 	}
 
 	op := binary.BigEndian.Uint16(packet[:2])
 	if !(1 <= op && op <= 5) {
-		return OpInvalid, fmt.Errorf("%d is not a valid op code: %w", op, errInvalidOp)
+		return OpInvalid, fmt.Errorf("%d is not a valid op code: %w", op, ErrInvalidOperation)
 	}
 
 	return Op(op), nil
@@ -66,9 +91,9 @@ const (
 	ModeOctet    Mode = "octet"
 )
 
-// packet structure: op:2 - filename->0 - mode->0
-func FromRWRequest(op Op, filename string, mode Mode) []byte {
-	packet := make([]byte, opPacketByteSize+len(filename)+1+len(mode)+1)
+// rwrequest packet structure: op:2 - filename->0 - mode->0
+func NewRWRequest(op Op, filename string, mode Mode) []byte {
+	packet := make([]byte, 2+len(filename)+1+len(mode)+1)
 
 	binary.BigEndian.PutUint16(packet, uint16(op))
 	n := 2
@@ -90,29 +115,128 @@ type RWRequest struct {
 	Mode     Mode
 }
 
-// packet structure: op:2 - filename->0 - mode->0
+// rwrequest packet structure: op:2 - filename->0 - mode->0
 // RWRequestFrom Extracts the fields from either the read or write request. Aside from the op code,
 // Read and Write requests contain the same data.
 func RWRequestFrom(packet []byte) (RWRequest, error) {
 	fields := bytes.Split(packet[2:], []byte{0})
 	if len(fields) < 2 {
-		return RWRequest{}, fmt.Errorf("missing filename or mode: %w", errMalformedPacket)
+		return RWRequest{}, Error{0, "missing_filename_or_mode"}
 	}
 	if len(fields[0]) < 1 {
-		return RWRequest{}, fmt.Errorf("missing or invalid filename: %w", errMalformedPacket)
+		return RWRequest{}, Error{0, "missing_or_invalid_filename"}
 	}
 	if len(fields[1]) < 1 {
-		return RWRequest{}, fmt.Errorf("missing mode: %w", errMalformedPacket)
+		return RWRequest{}, Error{0, "missing_or_invalid_mode"}
 	}
 
 	filename := string(fields[0])
 	mode := Mode(string(fields[1]))
 	if !(mode == ModeOctet || mode == ModeNetascii) {
-		return RWRequest{}, fmt.Errorf("invalid mode: %w", errMalformedPacket)
+		return RWRequest{}, Error{0, "missing_mode"}
 	}
 
 	return RWRequest{
 		Mode:     mode,
 		Filename: filename,
 	}, nil
+}
+
+// data packet structure: op:2 - block:2 - data:tsize
+// ack packet structure: op:2 - block:2
+func BlockFrom(packet []byte) (int, error) {
+	if len(packet) < 4 {
+		return 0, Error{0, "missing_block_number"}
+	}
+	return int(binary.BigEndian.Uint16(packet[2:4])), nil
+}
+
+// data packet structure: op:2 - block:2 - data:tsize
+func DataFrom(packet []byte) ([]byte, error) {
+	if len(packet) < 4 {
+		return nil, Error{0, "missing_data"}
+	}
+	return packet[4:], nil
+}
+
+// data packet structure: op:2 - block:2 - data:tsize
+func NewData(block int, data []byte) []byte {
+	packet := make([]byte, 2+2+len(data))
+
+	binary.BigEndian.PutUint16(packet, uint16(3))
+	n := 2
+
+	binary.BigEndian.PutUint16(packet[n:], uint16(block))
+	n += 2
+
+	n += copy(packet[n:], data)
+
+	return packet
+}
+
+// ack packet structure: op:2 - size:2 - data:size
+func NewAck(block int) []byte {
+	packet := make([]byte, 2+2)
+
+	binary.BigEndian.PutUint16(packet, uint16(4))
+	n := 2
+	binary.BigEndian.PutUint16(packet[n:], uint16(block))
+
+	return packet
+}
+
+type Error struct {
+	Code    int
+	Message string
+}
+
+func (e Error) String() string {
+	return fmt.Sprintf("%d %s", e.Code, e.Message)
+}
+
+func (e Error) Error() string {
+	return e.String()
+}
+
+// error packet structure: op:2 - error_code:2 - message->0
+func ErrorFrom(packet []byte) (Error, error) {
+	if len(packet) < 4 {
+		return Error{}, errors.New("malformed_packet")
+	}
+
+	code := int(binary.BigEndian.Uint16(packet[2:4]))
+	if len(packet) < 5 {
+		return Error{Code: code}, nil
+	}
+
+	sentinel := 4
+	for ; sentinel < len(packet) && packet[sentinel] != 0; sentinel++ {
+	}
+	return Error{
+		Code:    code,
+		Message: string(packet[4:sentinel]),
+	}, nil
+}
+
+// error packet structure: op:2 - error_code:2 - message->0
+func NewError(err error) []byte {
+	code := 0
+	msg := err.Error()
+	if pErr, ok := err.(Error); ok {
+		code = pErr.Code
+		msg = pErr.Message
+	}
+	msgBytes := []byte(msg)
+	packet := make([]byte, 2+2+len(msgBytes)+1)
+
+	binary.BigEndian.PutUint16(packet, uint16(OpError))
+	n := 2
+	binary.BigEndian.PutUint16(packet[n:], uint16(code))
+	n += 2
+
+	n += copy(packet[n:], msgBytes)
+	packet[n] = 0
+	n++
+
+	return packet
 }
--- packet/packet_test.go
+++ packet/packet_test.go
@@ -1,10 +1,12 @@
 package packet
 
 import (
+	"errors"
+	"strings"
 	"testing"
 )
 
-func TestRequest_Packing(t *testing.T) {
+func TestRWRequest(t *testing.T) {
 	type testCase struct {
 		op       Op
 		filename string
@@ -18,7 +20,7 @@ func TestRequest_Packing(t *testing.T) {
 		{OpRead, "/0_0", ModeNetascii},
 	}
 	for _, tc := range testCases {
-		packet := FromRWRequest(tc.op, tc.filename, tc.mode)
+		packet := NewRWRequest(tc.op, tc.filename, tc.mode)
 		op, err := OpFrom(packet)
 		if err != nil {
 			t.Fatal("problem when getting op from packet: ", err)
@@ -35,6 +37,110 @@ func TestRequest_Packing(t *testing.T) {
 		}
 		if wr.Mode != tc.mode {
 			t.Fatalf("mode: expected=\"%s\" actual=\"%s\"", tc.mode, wr.Mode)
+		}
+	}
+}
+
+func TestData(t *testing.T) {
+	type testCase struct {
+		block int
+		data  []byte
+	}
+
+	testCases := []testCase{
+		{0, []byte{0x1, 0x2, 0x3, 0x4, 0x5}},
+		{1, []byte{0x1}},
+		{2, []byte{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}},
+	}
+	for _, tc := range testCases {
+		packet := NewData(tc.block, tc.data)
+		op, err := OpFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting op from packet:", err)
+		}
+		if op != OpData {
+			t.Fatal("expected data op but got ", op)
+		}
+		block, err := BlockFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting block from packet:", err)
+		}
+		if block != tc.block {
+			t.Fatalf("block: expected=%d actual=%d", tc.block, block)
+		}
+		data, err := DataFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting data from packet:", err)
+		}
+		if len(data) != len(tc.data) {
+			t.Fatalf("data: expected=%v actual=%v", tc.data, data)
+		}
+		for i := range data {
+			if data[i] != tc.data[i] {
+				t.Fatalf("data: expected=%v actual=%v", tc.data, data)
+			}
+		}
+	}
+}
+
+func TestAck(t *testing.T) {
+	type testCase struct {
+		block int
+	}
+
+	testCases := []testCase{
+		{0},
+		{1},
+		{2},
+	}
+	for _, tc := range testCases {
+		packet := NewAck(tc.block)
+		op, err := OpFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting op from packet:", err)
+		}
+		if op != OpAck {
+			t.Fatal("expected ack op but got ", op)
+		}
+		block, err := BlockFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting block from packet:", err)
+		}
+		if block != tc.block {
+			t.Fatalf("block: expected=%d actual=%d", tc.block, block)
+		}
+	}
+}
+
+func TestError(t *testing.T) {
+	type testCase struct {
+		expectedCode int
+		err          error
+	}
+
+	testCases := []testCase{
+		{0, errors.New("something_unexpected")},
+		{1, ErrFileNotFound},
+		{2, ErrAccessViolation},
+	}
+	for _, tc := range testCases {
+		packet := NewError(tc.err)
+		op, err := OpFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting op from packet:", err)
+		}
+		if op != OpError {
+			t.Fatal("expected error op but got ", op)
+		}
+		pErr, err := ErrorFrom(packet)
+		if err != nil {
+			t.Fatal("problem when getting error from packet:", err)
+		}
+		if pErr.Code != tc.expectedCode {
+			t.Fatalf("code: expected=%d actual=%d", tc.expectedCode, pErr.Code)
+		}
+		if !strings.Contains(tc.err.Error(), pErr.Message) {
+			t.Fatalf("message: expected=%d actual=%d", tc.expectedCode, pErr.Code)
 		}
 	}
 }
